281b221195aed31c08cd67627bfa02f0a6b17aeb
Oliver Browning <lemenade3@hotmail.com>
2022-09-27 23:36:59 +0100

    added js validation and refactored app logic

--- script.js
+++ script.js
@@ -1,99 +1,134 @@
 let myLibrary = [];
 
+let serial = 0
+
 class Book {
-    constructor(name, author, pages, read) {
+    constructor(name, author, pages, read, serial) {
         this.name = name;
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.serial = serial
     }
-
-};
-
-function addToLibrary(name) {
-    name = document.querySelector('#name').value;
-    let author = document.querySelector('#author').value;
-    let pages = document.querySelector('#pages').value;
-    let read = document.querySelector('#read').checked;
-    name = new Book(name, author, pages, read)
-    myLibrary.push(name);
 };
 
 let modal = document.querySelector('#cardModal');
 
 let addBook = document.querySelector('#newCard');
-
-let span = document.querySelector('.close');
-
 addBook.addEventListener('click', function() {
     modal.style.display = 'block';
 });
 
+let span = document.querySelector('.close');
 span.addEventListener('click', function () {
     modal.style.display = 'none';
 });
 
-window.addEventListener('click', function (event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
+let commitCard = document.querySelector('#commitCard');
+commitCard.addEventListener('click', function () {
+    validate();
 });
 
-let removeButton;
+function validate() {
+    let name = document.querySelector('#name');
+    if (name.validity.valueMissing) {
+        name.setCustomValidity("Please fill in the book's Name")
+        name.reportValidity();
+    } else {
+        name.setCustomValidity("")
+    }
+
+    let author = document.querySelector('#author');
+    if (author.validity.valueMissing) {
+        author.setCustomValidity("Please fill in the book's Author")
+        author.reportValidity();
+    } else {
+        author.setCustomValidity("")
+    }
+
+    let pages = document.querySelector('#pages');
+    if (pages.validity.valueMissing) {
+        pages.setCustomValidity("Please fill in how many pages this book has")
+        pages.reportValidity();
+    } else if (pages.validity.rangeUnderflow) {
+        pages.setCustomValidity("If there is less than one page, it's not really a book")
+        pages.reportValidity();
+    } else {
+        pages.setCustomValidity("")
+    }
+
+    if (name.validity.valid && author.validity.valid && pages.validity.valid) {
+        commit();
+    };
+};
+
+function clearInputs() {
+    document.querySelector('#name').value = ''
+    document.querySelector('#author').value = ''
+    document.querySelector('#pages').value = ''
+    document.querySelector('#read').checked = false
+}
+
+function commit() {
+    let book = new Book(document.querySelector('#name').value, document.querySelector('#author').value, document.querySelector('#pages').value, document.querySelector('#read').checked, serial)
+    myLibrary.push(book)
+    serial += 1;
+    console.log(serial)
+    displayLibrary();
+    clearInputs()
+    modal.style.display = 'none';
+}
 
 function displayLibrary () {
-    library.textContent = '';
+    document.querySelector('#library').innerHTML = ''
     for (let i = 0; i < myLibrary.length; i++) {
+        writeBook(myLibrary[i]);
+    };
+};
 
-        function changeRead () {
-            if (myLibrary[i].read == true) {
-                myLibrary[i].read = false;
-            } else if (myLibrary[i].read == false) {
-                myLibrary[i].read = true;
-            };
-        };
+function writeBook(book) {
+    const library = document.querySelector('#library');
 
-        const card = document.createElement('div');
+    const card = document.createElement('div');
         card.setAttribute('class', 'card');
-        card.setAttribute('id', i)
 
-        removeButton = document.createElement('button');
+        let removeButton = document.createElement('button');
         removeButton.setAttribute('class', 'removeButton');
         removeButton.textContent = 'Delete';
-
-        removeButton.addEventListener('click', function () {
-            myLibrary.splice(i, 1);
+        removeButton.addEventListener('click', () => {
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].serial === book.serial) {
+                    myLibrary.splice(i, 1);
+                }
+            }
             displayLibrary();
         });
 
         const storedName = document.createElement('div');
         storedName.setAttribute('class', 'storedName');
-        storedName.textContent = myLibrary[i].name;
+        storedName.textContent = book.name;
 
         const storedAuthor = document.createElement('div');
         storedAuthor.setAttribute('class', 'storedAuthor');
-        storedAuthor.textContent = myLibrary[i].author;
+        storedAuthor.textContent = book.author;
 
         const storedPages = document.createElement('div');
         storedPages.setAttribute('class', 'storedPages');
-        storedPages.textContent = `Pages: ${myLibrary[i].pages}`;
+        storedPages.textContent = `Pages: ${book.pages}`;
 
         const storedRead = document.createElement('input');
+        storedRead.setAttribute('type', 'checkbox');
+        storedRead.setAttribute('class', 'storedRead');
+        storedRead.checked = book.read;
+        storedRead.addEventListener('change', () => {
+            book.read = storedRead.checked;
+        });
 
         const switchLabel = document.createElement('label');
         switchLabel.setAttribute('class', 'switch');
 
         const sliderRound = document.createElement('span');
         sliderRound.setAttribute('class', 'slider round');
-
-        storedRead.setAttribute('type', 'checkbox');
-        storedRead.setAttribute('class', 'storedRead');
-        storedRead.addEventListener('change', function() {
-            changeRead();
-        });
-
-        const checkRead = myLibrary[i].read;
-        storedRead.checked = checkRead;
 
         const topCard = document.createElement('div');
         topCard.setAttribute('class', 'topCard')
@@ -103,20 +138,5 @@ function displayLibrary () {
 
         card.append(topCard, storedAuthor, storedPages, switchLabel);
         library.append(card);
-    };
-};
+}
 
-let newName = document.querySelector("#name");
-
-let commitCard = document.querySelector('#commitCard');
-commitCard.addEventListener('click', function () {
-    addToLibrary();
-    displayLibrary();
-    modal.style.display = 'none';
-    newName.value = '';
-    author.value = '';
-    pages.value = '';
-    read.checked = false;
-});
-
-displayLibrary();
